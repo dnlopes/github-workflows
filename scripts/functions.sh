39fa983395bfa9297b1dc9217059f3390e8d1bba
David Lopes <me@davlopes.com>
2023-12-07 23:10:45 +0000

    feat: add primitive to assume automation role on a given account

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -4,6 +4,8 @@ set -e
 currentDir="$(dirname "$(readlink -f "$0")")"
 source $(dirname "$(readlink -f "$0")")/pretty_print.sh
 
+AUTOMATION_ROLE_NAME="IaCResourcesManager"
+
 assumeRole() {
     if [ "$1" = "" ]; then printRed "usage: assumeRole <aws-account> <role-name>" && exit 1; fi
     if [ "$2" = "" ]; then printRed "usage: assumeRole <aws-account> <role-name>" && exit 1; fi
@@ -15,4 +17,9 @@ assumeRole() {
     $(aws sts assume-role --role-arn arn:aws:iam::$1:role/$2 --role-session-name $roleSession \
     --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" --no-cli-pager --output text))
     printGreen "Role assumed"
+}
+
+assumeAutomationRoleOnAccount() {
+    if [ "$1" = "" ]; then printRed "usage: assumeAutomationRoleOnAccount <aws-account>" && exit 1; fi
+    assumeRole "$1" "IaCResourcesManager"
 }
